be99ad9d414850abad2198d8c45995381ea97ffe
dev <dev@develop.com>
2020-07-02 13:13:20 +0530

    cahnges

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,8 +1,9 @@
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php
         meta_tags();
         ?>
